99c32c46aa605faca7fa89eb8a604d5e5a1281f7
Gabriel Cyr <cyr.gabriel.087@gmail.com>
2020-03-28 15:02:16 -0400

    First commit - base Spring project

---
+++ src/main/java/net/gabsire087/springgradleopenapidemo/SpringGradleOpenapiDemoApplication.java
@@ -0,0 +1,13 @@
+package net.gabsire087.springgradleopenapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleOpenapiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradleOpenapiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/gabsire087/springgradleopenapidemo/SpringGradleOpenapiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gabsire087.springgradleopenapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleOpenapiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
